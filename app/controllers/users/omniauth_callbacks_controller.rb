a39bb31b351f7d7917960d3b40cc5c2a50b0fdd4
Ana Khadivi <anakhadivi@icloud.com>
2014-04-08 13:16:24 -0400

    added columns

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,12 +1,12 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 	def twitter
 		puts request.env['omniauth.auth']
-		@user = User.find_for_facebook(request.env['omniauth.auth'])
+		@user = User.find_for_twitter_oauth(request.env['omniauth.auth'])
 		if @user.persisted?
 			sign_in_and_direct @user, :event => :authentication
 			set_flash_message(:notice, :success, :kind => 'Twitter') if is navigational format?
 		else
-			session['devise.user_attributes'] = @user.user_attributes
+			session['devise.user_attributes'] = @user.attributes
 			redirect_to new_user_registration_url
 		end
 	end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,12 @@ class User < ActiveRecord::Base
   	devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:twitter]
     def self.find_for_twitter_oauth(auth)
-   		where(auth.slice(:provider, :uid)).first_or_create do |user|
-   			user.provider = auth.provider
-   			user.uid = auth.uid
-   			user.email = auth.info.email
-   			user.password = Devise.friendly_token[0,20]
-   		end
+  where(auth.slice(:provider, :uid)).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+  end
    	end
 end
 
